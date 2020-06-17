fba330e889ada14989eee504118f51ed723c6586
PureGero <puregero@gmail.com>
2020-06-17 17:10:23 +1000

    Initial commit

---
+++ src/main/java/just/plots/JustPlots.java
@@ -0,0 +1,17 @@
+package just.plots;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class JustPlots extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+}
